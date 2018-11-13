f7f88cb9fbaf898ca9efdfe8d47cea2971995fc2
dinushadck <dinusha@duosoftware.com>
2018-11-13 11:43:47 +0530

    set default config

--- config/default.js
+++ config/default.js
@@ -1,78 +1,78 @@
 module.exports = {
 
-    "DB": {
-        "Type":"postgres",
-        "User":"duo",
-        "Password":"DuoS123",
-        "Port":5432,
-        "Host":"104.236.231.11",
-        "Database":"duo"
-    },
+  "DB": {
+    "Type":"postgres",
+    "User":"",
+    "Password":"",
+    "Port":5432,
+    "Host":"",
+    "Database":""
+  },
 
-    "Redis":
-        {
-            "mode":"sentinel",//instance, cluster, sentinel
-            "ip": "45.55.142.207",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
-            "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
-                "name":"redis-cluster"
-            }
+  "Redis":
+  {
+    "mode":"instance",//instance, cluster, sentinel
+    "ip": "",
+    "port": 6389,
+    "user": "",
+    "password": "",
+    "sentinels":{
+      "hosts": "",
+      "port":16389,
+      "name":"redis-cluster"
+    }
 
-        },
+  },
 
-    "Security":
-        {
+  "Security":
+  {
 
-            "ip" : "45.55.142.207",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
-            "mode":"sentinel",//instance, cluster, sentinel
-            "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
-                "name":"redis-cluster"
-            }
-        },
+    "ip" : "",
+    "port": 6389,
+    "user": "",
+    "password": "",
+    "mode":"instance",//instance, cluster, sentinel
+    "sentinels":{
+      "hosts": "",
+      "port":16389,
+      "name":"redis-cluster"
+    }
+  },
 
-    "RabbitMQ":
-        {
-            "ip": "45.55.142.207",
-            "port": 5672,
-            "user": "admin",
-            "password": "admin",
-            "vhost":'/'
-        },
+  "RabbitMQ":
+  {
+    "ip": "",
+    "port": 5672,
+    "user": "",
+    "password": "",
+    "vhost":'/'
+  },
 
-    "Mongo":
-        {
-            "ip":"104.236.231.11",
-            "port":"27017",
-            "dbname":"dvpdb",
-            "password":"DuoS123",
-            "user":"duo",
-            "replicaset" :"104.236.231.11"
-        },
+  "Mongo":
+  {
+    "ip":"",
+    "port":"27017",
+    "dbname":"",
+    "password":"",
+    "user":"duo",
+    "replicaset" :""
+  },
 
-    "Services":
-        {
+  "Services":
+  {
 
-            "fileServiceHost": "fileservice.app.veery.cloud",
-            "fileServicePort": 5649,
-            "fileServiceVersion":"1.0.0.0"
+    "fileServiceHost": "",
+    "fileServicePort": 5649,
+    "fileServiceVersion":"1.0.0.0"
 
-        },
+  },
 
-    "Host":{
-        "Ip":"0.0.0.0",
-        "Port":9093,
-        "Version":"1.0.0.0"
-    },
-    "AbandonCallThreshold": 5,
-    "SaveRawCDRMongo": "true",
-    "Token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
+  "Host":{
+    "Ip":"0.0.0.0",
+    "Port":9093,
+    "Version":"1.0.0.0"
+  },
+  "AbandonCallThreshold": 5,
+  "SaveRawCDRMongo": "true",
+  "Token":""
 };
