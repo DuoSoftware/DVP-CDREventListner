8062fb0ec0b0f597dc2fe39128b4cf6b0b66093c
sukitha jayasingha <sukithaj@gmail.com>
2020-02-22 17:15:36 +0530

    amqp log

--- AMQPHandler.js
+++ AMQPHandler.js
@@ -1,16 +1,16 @@
-var format = require('stringformat');
-var config = require('config');
-var amqp = require('amqp');
-var logger = require('dvp-common/LogHandler/CommonLogHandler.js').logger;
+var format = require("stringformat");
+var config = require("config");
+var amqp = require("amqp");
+var logger = require("dvp-common/LogHandler/CommonLogHandler.js").logger;
 
 var ips = [];
 
-if(config.RabbitMQ.ip) {
-    ips = config.RabbitMQ.ip.split(",");
+if (config.RabbitMQ.ip) {
+  ips = config.RabbitMQ.ip.split(",");
 }
 
-
-var queueConnection = amqp.createConnection({
+var queueConnection = amqp.createConnection(
+  {
     //url: queueHost,
     host: ips,
     port: config.RabbitMQ.port,
@@ -18,34 +18,36 @@ var queueConnection = amqp.createConnection({
     password: config.RabbitMQ.password,
     vhost: config.RabbitMQ.vhost,
     noDelay: true,
-    heartbeat:10
-}, {
+    heartbeat: 10
+  },
+  {
     reconnect: true,
-    reconnectBackoffStrategy: 'linear',
+    reconnectBackoffStrategy: "linear",
     reconnectExponentialLimit: 120000,
     reconnectBackoffTime: 1000
+  }
+);
+
+queueConnection.on("ready", function() {
+  logger.info("Conection with the queue is OK");
 });
 
-queueConnection.on('ready', function () {
-
-    logger.info("Conection with the queue is OK");
+queueConnection.on("error", function(error) {
+  logger.info("There is an error" + error);
 });
 
-queueConnection.on('error', function (error) {
-
-    logger.info("There is an error" + error);
+queueConnection.on("heartbeat", function() {
+  logger.debug("RabbitMQ HeartBeat");
 });
 
-module.exports.PublishToQueue = function(messageType,sendObj ) {
-
-    try {
-        if (sendObj) {
-            queueConnection.publish(messageType, sendObj, {
-                contentType: 'application/json'
-            });
-        }
-    } catch (exp) {
-
-        console.log(exp);
+module.exports.PublishToQueue = function(messageType, sendObj) {
+  try {
+    if (sendObj) {
+      queueConnection.publish(messageType, sendObj, {
+        contentType: "application/json"
+      });
     }
+  } catch (exp) {
+    console.log(exp);
+  }
 };
